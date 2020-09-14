84dee2ab85fe8d81b1e6f951f2d77c879360a7ca
pankaj8blr <pankaj8blr@gmail.com>
2020-09-14 12:58:02 +0530

    first commit from master

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
